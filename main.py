189f89542e05e388af77ed5809e099d5adfb90bc
sohamjitendra <sohamjitendra2k9@gmail.com>
2025-08-24 20:36:49 +0530

    a new feature

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+# Generate a random number between 1 and 10
+secret = random.randint(1, 10)
+
+print("Guess the number between 1 and 10!")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    if guess == secret:
+        print("🎉 Correct! You guessed it.")
+        break
+    elif guess < secret:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
