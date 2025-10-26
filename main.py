0aa3d3858f9290be7429229384afdb8fa6ec7c7c
sohamjitendra <sohamjitendra2k9@gmail.com>
2025-10-26 16:34:22 +0530

    first commit

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    data = request.get_json()
-    name = data.get("name", "Guest")
-    return jsonify({"message": f"Hello, {name}! Welcome to Flask + JS demo 🚀"})
-
-if __name__ == "__main__":
-    app.run(debug=True)
-=======
 from flask import Flask, render_template, request
 from PIL import Image
 import os
